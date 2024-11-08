373eb28c5772830e483aee896188425c45559759
Andrew Kauffman <andrewkauffman15@gmail.com>
2024-11-08 10:54:39 -0500

    replaced underscore with dash, improved setup script, removed Makefile

--- README.md
+++ README.md
@@ -1,31 +1,13 @@
 # python-template
 
-This is a template python repository that allows for installation with pyproject.toml, has a cli script set up, and has some Makefile commands for easy project initialization.
-
-## Setup
-
-Use Makefile to set up virtual environment:
-
-`
-make virtualenv
-`
-
-Then, install the virtual environment with the printed command. Then install the project in development mode:
-
-`
-make install
-`
+This is a template python repository that allows for installation with pyproject.toml and has a cli script set up
 
 ## Using
 
 The template is set up to be used as a module, but the cli setup allows for direct calls using:
 
-`
-python -m python-template
-`
+`python -m python-template`
 
 OR
 
-`
-python-template
-`
+`python-template`
--- setup.sh
+++ setup.sh
@@ -4,14 +4,44 @@ cd $(dirname $0)
 
 remote_url=$(git config --get remote.origin.url)
 repo_name=$(basename -s .git "$remote_url")
-project=$(echo "$repo_name" | sed 's/[^a-zA-Z0-9]/_/g')
-project_cli="$(echo "$project" | sed 's/_/-/g')"
+project=$(echo "$repo_name" | sed 's/[^a-zA-Z0-9]/-/g')
 
-files=("src/python_template/base.py" "src/python_template/cli.py" "pyproject.toml" "README.md")
+files=("src/python-template/base.py" "src/python-template/cli.py" "README.md")
 
 for file in "${files[@]}"; do
-    sed -i "s/python_template/$project/g" "$file"
-    sed -i "s/python-template/$project_cli/g" "$file"
+    sed -i "s/python-template/$project/g" "$file"
 done
 
-mv src/python_template src/$project
+cat <<EOF > pyproject.toml
+[build-system]
+requires = ["setuptools>=61.0"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "$project"
+version = "0.0.1"
+authors = [
+  { name="$(git config --get user.name)", email="$(git config --get user.email)" },
+]
+description = "python template repository"
+readme = "README.md"
+requires-python = ">=3.8"
+classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: UNLICENSED",
+  "Operating System :: OS Independent",
+]
+
+dependencies = []
+
+[project.optional-dependencies]
+dev = [
+  "pytest",
+  "devpi-client"
+]
+
+[project.scripts]
+$project = "$project.cli:main"
+EOF
+
+mv src/python-template src/$project
