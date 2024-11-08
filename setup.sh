a955d4f9591123ab23d9642344d63e12b0922dd3
Andrew Kauffman <andrewkauffman15@gmail.com>
2024-11-08 12:40:20 -0500

    had to move back to underscores

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@ cd $(dirname $0)
 
 remote_url=$(git config --get remote.origin.url)
 repo_name=$(basename -s .git "$remote_url")
-project=$(echo "$repo_name" | sed 's/[^a-zA-Z0-9]/-/g')
+project=$(echo "$repo_name" | sed 's/[^a-zA-Z0-9]/_/g')
+project_cli=$(echo "$project" | sed 's/_/-/g')
+read -p "package description: " description
 
-files=("src/python-template/base.py" "src/python-template/cli.py" "README.md")
+files=("src/python_template/cli.py" "README.md")
 
 for file in "${files[@]}"; do
-    sed -i "s/python-template/$project/g" "$file"
+    sed -i "s/python-template/$project_cli/g" "$file"
+    sed -i "s/python_template/$project/g" "$file"
 done
 
 cat <<EOF > pyproject.toml
@@ -23,7 +26,7 @@ version = "0.0.1"
 authors = [
   { name="$(git config --get user.name)", email="$(git config --get user.email)" },
 ]
-description = "python template repository"
+description = "$description"
 readme = "README.md"
 requires-python = ">=3.8"
 classifiers = [
@@ -41,7 +44,7 @@ dev = [
 ]
 
 [project.scripts]
-$project = "$(echo $project | sed 's/-/_/g').cli:main"
+$project_cli = "$project.cli:main"
 EOF
 
-mv src/python-template src/$project
+mv src/python_template src/$project
